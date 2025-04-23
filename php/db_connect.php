b5b27cd5fb91662cf83f0a78b63ec057c878363d
József Dániel Dombi <dombijd@inf.u-szeged.hu>
2025-04-23 21:58:12 +0200

    try to debug

--- php/db_connect.php
+++ php/db_connect.php
@@ -4,11 +4,19 @@ $username = "annAdmin";
 $password = "FvhrFnjHzgF32!";
 $dbname = "szakdoga";
 
+print $servername
+print $username
+print $password
+print $szakdoga
+
 // Kapcsolódás az adatbázishoz
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+print $conn
+
 // Ellenőrizzük, hogy a kapcsolat sikerült-e
 if ($conn->connect_error) {
+    print $conn->connect_error
     die("Kapcsolódás hiba: " . $conn->connect_error);
 }
 ?>
