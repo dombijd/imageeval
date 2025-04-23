bd6cc72e47db9324f5343168e2b9b16234599007
József Dániel Dombi <dombijd@inf.u-szeged.hu>
2025-04-23 22:52:01 +0200

    fix syntax error

--- php/db_connect.php
+++ php/db_connect.php
@@ -12,11 +12,8 @@ echo $szakdoga;
 // Kapcsolódás az adatbázishoz
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-print $conn
-
 // Ellenőrizzük, hogy a kapcsolat sikerült-e
 if ($conn->connect_error) {
-    echo $conn->connect_error;
     die("Kapcsolódás hiba: " . $conn->connect_error);
 }
 ?>
