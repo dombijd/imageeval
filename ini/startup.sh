575fb3cd94f53fef7ea50eb86ad512c73ff3d2b7
József Dániel Dombi <dombijd@inf.u-szeged.hu>
2025-04-28 18:33:21 +0200

    fix space issue

--- ini/startup.sh
+++ ini/startup.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 
- echo "copying over zz-docker.conf.."
- cp /home/zz-docker.conf /usr/local/etc/php-fpm.d/zz-docker.conf
+echo "copying over zz-docker.conf.."
+cp /home/zz-docker.conf /usr/local/etc/php-fpm.d/zz-docker.conf
